4e750eca5216fc60a92c7c40fe15026f6f32f517
zm412 <zm412412@gmail.com>
2020-10-08 11:27:25 -0400

    just little mark

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Block2 from '../src/components/Block2';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import AuthInfo from '../src/components/Auth';
 import Button from '@material-ui/core/Button';
 import Router from 'next/router';
 import Link from 'next/link'
